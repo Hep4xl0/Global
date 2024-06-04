19e49c8120226d133c5b1685406aa821cfaa5a66
Hep4xl0 <152357440+Hep4xl0@users.noreply.github.com>
2024-06-04 03:00:30 -0300

    GS

---
+++ Trabalho.py
@@ -0,0 +1,12 @@
+total = 150.000.000
+paises = [{'Pais' : 'Eua', 'Porcentagem' : 1 },
+          {'Pais' : 'Filipinas', 'Porcentagem' : 5 },
+          {'Pais' : 'Brasil', 'Porcentagem' : 1 },
+          {'Pais' : 'India', 'Porcentagem' : 3 },
+          {'Pais' : 'China', 'Porcentagem' : 30 },
+          {'Pais' : 'Indonesia', 'Porcentagem' : 10 }
+]
+
+EUA = [{'Empresa' : 'Coca-Cola', 'Poluição' : 27.7 ,'Empresa' : 'PepsiCo', 'Poluição'  : 20,'Empresa' : 'Nestlé', 'Poluição' : 12.73,'Empresa' : 'Unilever', 'Poluição' : 10,'Empresa' : 'Procter & Gamble', 'Poluição' : 8.18,'Empresa' : 'Mondelez International', 'Poluição' : 7.27,'Empresa' : 'Mars Incorporated', 'Poluição' : 5.45,'Empresa' : 'Colgate-Palmolive', 'Poluição' : 3.64,'Empresa' : 'Danone', 'Poluição' : 4.55,'Empresa' : 'Perfetti van Melle', 'Poluição' : 2.73,}]
+China = [{}]
+def
